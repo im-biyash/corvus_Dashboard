858be2d3d06b37cc3a165405df4aaff5d3f448c5
biyash <bsstha21@gmail.com>
2024-06-10 23:25:04 +0545

    content psge

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Sidebar from "../components/Sidebar"
 
+import Sidebar from "../components/Sidebar"
+import Content from "../components/Content"
 
 export default function Home() {
   return (
    <div className="flex h-screen items-center justify-between">
    <Sidebar/>
+   <Content/>
    </div>
   );
 }
---
+++ components/Content.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='p-2 min-h-screen flex items-start justify-center'>
+      <h1>hello im mian content</h1>
+    </div>
+  )
+}
+
+export default Content
