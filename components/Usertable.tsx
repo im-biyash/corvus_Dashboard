60b3628683cd235ed334941fb221ad6ee835d764
biyash <bsstha21@gmail.com>
2024-06-14 21:50:34 +0545

    change

--- components/Usertable.tsx
+++ components/Usertable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "./ui/button";
